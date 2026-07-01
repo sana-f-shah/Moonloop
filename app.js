db8ce0c74ab1f1c9bd21535709f6891078393266
sana-f-shah <sana.f.shah@outlook.com>
2026-07-02 00:49:41 +0100

    Build first playable Moonloop prototype

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
-let light = 0;
+let moonlight = 0;
+let moonstones = 0;
 
-const lightDisplay = document.getElementById("light");
-const button = document.getElementById("clickBtn");
+const moonlightDisplay = document.getElementById("moonlight");
 
-button.addEventListener("click", () => {
-  light++;
-  lightDisplay.textContent = "Light: " + light;
-});
+const gatherButton = document.getElementById("gatherButton");
+const moonstoneButton = document.getElementById("buyMoonstone");
+
+gatherButton.addEventListener("click", gatherMoonlight);
+moonstoneButton.addEventListener("click", restoreMoonstone);
+
+function gatherMoonlight() {
+    moonlight++;
+    updateDisplay();
+}
+
+function restoreMoonstone() {
+
+    if (moonlight >= 10) {
+
+        moonlight -= 10;
+        moonstones++;
+
+        alert("The first Moonstone has awakened.");
+
+        updateDisplay();
+
+    } else {
+
+        alert("You need more Moonlight.");
+
+    }
+
+}
+
+function updateDisplay() {
+
+    moonlightDisplay.textContent = moonlight;
+
+}
